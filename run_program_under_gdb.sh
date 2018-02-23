9987667f098694f16ef6a0fd07d0cda3e251e6bf
Rafal Kruszyna <rafal-tarnow@wp.pl>
2018-02-23 09:05:05 +0100

    first version of script

---
+++ run_program_under_gdb.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo $1
+
+DATE=`date '+%Y-%m-%d %H:%M:%S'`
+
+printf "\n\n" >> log.txt
+echo "************************************" >> log.txt
+echo "******* START Debug Session ********" >> log.txt
+echo "****** " $DATE " *******" >> log.txt
+echo "************************************" >> log.txt
+printf "\n" >> log.txt
+
+gdb -q -batch -ex run $1 >> log.txt
+
+printf "\n" >> log.txt
+echo "************************************" >> log.txt
+echo "******* STOP Debug Session ********" >> log.txt
+echo "************************************" >> log.txt
+printf "\n" >> log.txt
