3c76a3666a0985365d621377062ffe46209947aa
Rafal Kruszyna <rafal-tarnow@wp.pl>
2018-02-23 10:43:29 +0100

    dorobienie wyrzucania calego stosu

--- run_program_under_gdb.sh
+++ run_program_under_gdb.sh
@@ -4,18 +4,20 @@
 # $2 - nazwa pliku do ktorego zostanie zrobiony log
 
 DATE=`date '+%Y-%m-%d %H:%M:%S'`
-
 echo "************************************" >> $2
 echo "******* START Debug Session ********" >> $2
 echo "****** " $DATE " *******" >> $2
 echo "************************************" >> $2
 printf "\n" >> $2
 
-gdb -q -batch -ex run $1 >> $2
+gdb -q -batch -ex run $1 >> $2 \
+	      -ex bt >> $2
 
+DATE=`date '+%Y-%m-%d %H:%M:%S'`
 printf "\n" >> $2
 echo "************************************" >> $2
 echo "******* STOP Debug Session ********" >> $2
+echo "****** " $DATE " *******" >> $2
 echo "************************************" >> $2
 printf "\n\n\n\n" >> $2
 
